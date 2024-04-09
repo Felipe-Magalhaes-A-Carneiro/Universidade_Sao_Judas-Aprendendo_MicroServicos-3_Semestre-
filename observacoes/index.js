a122d9eb8125368d899f4a8920b98534d4f1d2fc
Felipe-Magalhaes-A-Carneiro <felipe.araujo.arq@outlook.com>
2024-04-08 21:44:51 -0300

    feat (index.js): Desenvolve uma reposta envolvendo o código de status HTTP

--- observacoes/index.js
+++ observacoes/index.js
@@ -6,11 +6,29 @@ const observacoesPorLembretesId = {};
 app.use(express.json());
 
 app.post('/lembretes/:id/observacoes', (req, res) => {
+    
+    //Gerar um novo identificador para a observação a ser inserida.
     const idObs = vvidv4();
+
+    //Extrair, do corpo da requisição, o texto da observação.
     const { texto } = req.body;
+
+    //Verificar se o id de lembrete existente na URL já existe na vase e está associado a uma coleção.
+    const observacoesDoLembrete = observacoesPorLembretesId [req.params.id] || []
+
+    //Adicionar a nova observaçao à coleção de observações recém obtida / criada:
+    observacoesDoLembrete.push({id: id0bs, texto});
+
+    // Fazer com que o identificador do lembrete existente na URL esteja associado a essa nova coleção alterada, na base de observações por id de lembrete:
+    observacoesPorLembretesId[req.params.id] = observacoesDoLembrete;
+    
+    // Devolver uma resposta ao usuário enolvendo o código de status HTTP e algum objeto de interesse, possivelmente a observação inserida ou, ainda, a coleção inteira de observações
+    res.status(201).send (observacoesDoLembrete);
+
 });
 
 app.get('/lembretes/:id/observacoes', (req, res) => {
+    res. send(observacoesPorLembretesId[req.params.id] || []);
 
 });
 
