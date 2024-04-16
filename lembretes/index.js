3c65ceec3a635c688c04de45480e6b9d07f5592b
Felipe-Magalhaes-A-Carneiro <felipe.araujo.arq@outlook.com>
2024-04-15 21:54:08 -0300

    feat(json): enviar e receber mensagem

--- lembretes/index.js
+++ lembretes/index.js
@@ -1,6 +1,7 @@
 //Apostila usada: '03_apostila_microservicoes'
 
 const express = require('express');
+const axios = require('axios');
 const app = express();
 app.use(express.json()); //Obs: Diferente da apostila, será usado o json em tudo, logo não terá o 'const bodyParser = require(body-parse);', na página 25 da apostila '03_apostila_microservicoes'
 const lembretes = {};
@@ -10,14 +11,28 @@ app.get('/lembretes', (req, res) => {
     res.send(lembretes);
 });
 
-app.post('/lembretes', (req, res) => {
+app.post('/lembretes', async (req, res) => {
     contador++;
     const { texto } = req.body;
     lembretes[contador] = {
         contador, texto
     };
+    //Disparando lembrete:
+    await axios.post('http://localhost:10000/eventos', {
+        tipo: "LembreteCriado",
+        dados: {
+            contador,
+            texto,
+        },
+    });
     res.status(201).send(lembretes[contador]); // Vá para o 'package.jason' da pasta lembretes, e faça no terminal (cd lembretes), em seguida faça 'npm start'. Em seguida utilize a extensão 'Thunder Client', utilize o GET e cole novamente o link localhost:4000/lembretes e dê um SEND
 });
+
+app.post("/eventos", (req, res) => {
+    console.log(req.body);
+    res.status(200).send({ msg: "ok" });
+});
+
 
 app.listen(4000, () => {
     console.log("Lembretes. Porta 4000.");
--- observacoes/index.js
+++ observacoes/index.js
@@ -1,12 +1,12 @@
 const express = require('express');
-const {v4: vvidv4} = require('vvid');
+const { v4: vvidv4 } = require('vvid');
 const app = express();
 const observacoesPorLembretesId = {};
 
 app.use(express.json());
 
 app.post('/lembretes/:id/observacoes', (req, res) => {
-    
+
     //Gerar um novo identificador para a observação a ser inserida.
     const idObs = vvidv4();
 
@@ -14,24 +14,30 @@ app.post('/lembretes/:id/observacoes', (req, res) => {
     const { texto } = req.body;
 
     //Verificar se o id de lembrete existente na URL já existe na vase e está associado a uma coleção.
-    const observacoesDoLembrete = observacoesPorLembretesId [req.params.id] || []
+    const observacoesDoLembrete = observacoesPorLembretesId[req.params.id] || []
 
     //Adicionar a nova observaçao à coleção de observações recém obtida / criada:
-    observacoesDoLembrete.push({id: id0bs, texto});
+    observacoesDoLembrete.push({ id: id0bs, texto });
 
     // Fazer com que o identificador do lembrete existente na URL esteja associado a essa nova coleção alterada, na base de observações por id de lembrete:
     observacoesPorLembretesId[req.params.id] = observacoesDoLembrete;
-    
+
     // Devolver uma resposta ao usuário enolvendo o código de status HTTP e algum objeto de interesse, possivelmente a observação inserida ou, ainda, a coleção inteira de observações
-    res.status(201).send (observacoesDoLembrete);
+    res.status(201).send(observacoesDoLembrete);
 
 });
 
 app.get('/lembretes/:id/observacoes', (req, res) => {
-    res. send(observacoesPorLembretesId[req.params.id] || []);
+    res.send(observacoesPorLembretesId[req.params.id] || []);
 
 });
 
-app.listen(5000, (() =>{
+app.post("/eventos", (req, res) => {
+    console.log(req.body);
+    res.status(200).send({ msg: "ok" });
+});
+
+
+app.listen(5000, (() => {
     console.log("Observações. Porta 5000.");
 }));
